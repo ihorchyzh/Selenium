a9ccae912b0bb2cdb0f77c8d59db27266c036a83
Ihor Chyzh <ichyzh@gmail.com>
2017-03-23 22:09:26 +0200

    Add test

---
+++ src/main/java/com/ihorchyzh/core/WebDriverTestBase.java
@@ -0,0 +1,36 @@
+package com.ihorchyzh.core;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ihorchyzh on 3/23/17.
+ */
+public abstract class WebDriverTestBase {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver/geckodriver");
+        // Init browser
+        driver = new FirefoxDriver();
+        // Max window
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+//    driver.quit(); browser
+//    driver.close(); tab
+
+}
---
+++ src/test/java/com/ihorchyzh/pages/GoogleSearchTest.java
@@ -0,0 +1,30 @@
+package com.ihorchyzh.pages;
+
+import com.ihorchyzh.core.WebDriverTestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by ihorchyzh on 3/23/17.
+ */
+public class GoogleSearchTest extends WebDriverTestBase {
+
+    @Test
+    public void searchTest() {
+        //Open page
+        driver.get("https://www.google.com");
+        //Find element by name
+        WebElement searchField = driver.findElement(By.name("q"));
+        //Sends "Selenium" text into an input field
+        searchField.sendKeys("Selenium");
+        searchField.submit();
+        //Find first link with specified result
+        WebElement seleniumLink = driver.findElement(By.xpath(".//*[@id='rso']/div/div/div[1]/div/h3/a"));
+        //Verify result
+        assertTrue(seleniumLink.getText().contains("Selenium"));
+    }
+
+}
